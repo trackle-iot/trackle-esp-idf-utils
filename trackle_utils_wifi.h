9f14cad157d0220ab8ac0f10d9d2bd067bde7b5e
flaz83 <flavio.ferrandi@gmail.com>
2022-04-14 10:55:35 +0200

    fix: disable wifi powersave

--- trackle_utils_wifi.h
+++ trackle_utils_wifi.h
@@ -10,10 +10,11 @@
 #define CHECK_WIFI_TIMEOUT 5000
 unsigned long timeout_connect_wifi = 0;
 static int s_retry_num = 0;
+esp_netif_t *sta_netif;
 
 static const char *WIFI_TAG = "trackle-utils-wifi";
 
-//return ESP_OK if wisi is provisioned (strlen ssid > 0), else ESP_FAIL
+// return ESP_OK if wisi is provisioned (strlen ssid > 0), else ESP_FAIL
 esp_err_t wifi_is_provisioned()
 {
     /* Get Wi-Fi Station configuration */
@@ -45,23 +46,23 @@ static void event_handler(void *arg, esp_event_base_t event_base,
         if (currentMode == WIFI_MODE_STA)
         {
             ESP_LOGI(WIFI_TAG, "Connecting to the AP");
-            xEventGroupSetBits(s_wifi_event_group, WIFI_TO_CONNECT_BIT); //connettiti
-            //esp_wifi_connect();
+            xEventGroupSetBits(s_wifi_event_group, WIFI_TO_CONNECT_BIT); // connettiti
+            // esp_wifi_connect();
             timeout_connect_wifi = millis();
         }
         else if (currentMode == WIFI_MODE_APSTA)
         {
             ESP_LOGI(WIFI_TAG, "APMode, not connecting....");
-            xEventGroupClearBits(s_wifi_event_group, WIFI_TO_CONNECT_BIT); //non cercare di riconnetterti
+            xEventGroupClearBits(s_wifi_event_group, WIFI_TO_CONNECT_BIT); // non cercare di riconnetterti
         }
     }
     else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
     {
         wifi_event_sta_disconnected_t *event = (wifi_event_sta_disconnected_t *)event_data;
         ESP_LOGW(WIFI_TAG, "Wifi disconnection event: %d...", event->reason);
-        //wifi_err_reason_t
+        // wifi_err_reason_t
 
-        //esp_wifi_connect();
+        // esp_wifi_connect();
         timeout_connect_wifi = millis() + CHECK_WIFI_TIMEOUT;
         xEventGroupClearBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
     }
@@ -87,9 +88,10 @@ void wifi_init()
     ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
     ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
 
-    esp_netif_create_default_wifi_sta();
+    sta_netif = esp_netif_create_default_wifi_sta();
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    esp_wifi_set_storage(WIFI_STORAGE_FLASH);
 }
 
 /*static void reinit_wifi()
@@ -111,6 +113,7 @@ void wifi_init_sta()
 {
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_start());
+    esp_wifi_set_ps(WIFI_PS_NONE); // Disable powersave
     ESP_LOGI(WIFI_TAG, "wifi_init_sta finished.");
 }
 
@@ -119,7 +122,7 @@ void trackle_utils_wifi_loop()
     EventBits_t bits = xEventGroupGetBits(s_wifi_event_group);
     uint32_t loop_millis = millis();
 
-    //check connessione wifi
+    // check connessione wifi
     if (timeout_connect_wifi > 0 && timeout_connect_wifi < loop_millis)
     {
         timeout_connect_wifi = 0;
@@ -135,7 +138,7 @@ void trackle_utils_wifi_loop()
             {
                 ESP_LOGI(WIFI_TAG, "Restarting......");
                 s_retry_num = 0;
-                //reinit_wifi(); //TODO check if necessary
+                // reinit_wifi(); //TODO check if necessary
             }
         }
     }
