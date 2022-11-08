47a7595f9b52f9117accdf711ecaa818d3dfce3b
Federico Bombardieri <bombardieri.federico@gmail.com>
2022-11-08 10:16:58 +0100

    feat!: added gets from bt provisioning and renamed funcs to posts
    BREAKING CHANGE: Functions callable during BT provisioning are no longer called 'functions', now they are called 'posts', even in the interface's signatures.

--- trackle_utils_bt_functions.h
+++ trackle_utils_bt_functions.h
@@ -1,14 +1,20 @@
 #ifndef TRACKLE_UTILS_BT_FUNCTIONS_H
 #define TRACKLE_UTILS_BT_FUNCTIONS_H
 
-#define MAX_BT_FUNCTION_NAME_LEN 32
-#define MAX_BT_FUNCTIONS_NUM 10
+#define MAX_BT_POST_NAME_LEN 32
+#define MAX_BT_POSTS_NUM 10
+
+#define MAX_BT_GET_NAME_LEN 32
+#define MAX_BT_GETS_NUM 10
 
 #include <stdbool.h>
 
 #include <esp_err.h>
 
-bool Trackle_BtFunction_add(const char *name, int (*function)(const char *));
+#include <defines.h>
+
+bool Trackle_BtPost_add(const char *name, int (*function)(const char *));
+bool Trackle_BtGet_add(const char *name, void *(*function)(const char *), Data_TypeDef dataType);
 
 esp_err_t btFunctionsEndpointsCreate();
 esp_err_t btFunctionsEndpointsRegister();
