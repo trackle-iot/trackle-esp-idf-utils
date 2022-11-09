aad8ca5879c806e363a646adf5f63a3cdadb8b75
Federico Bombardieri <bombardieri.federico@gmail.com>
2022-11-09 17:20:24 +0100

    doc: moved doc files to doc folder

--- html/trackle__utils__ota_8h.js
+++
@@ -1,8 +0,0 @@
-var trackle__utils__ota_8h =
-[
-    [ "_http_event_handler", "trackle__utils__ota_8h.html#ae7c4f5057d30a650919c74c7f9e7d67c", null ],
-    [ "firmware_ota_url", "trackle__utils__ota_8h.html#ac2e187542a74410e3d1eabae0a6708c4", null ],
-    [ "simple_ota_task", "trackle__utils__ota_8h.html#ab11284ede385340cba8278bbc2df3f8e", null ],
-    [ "ota_url", "trackle__utils__ota_8h.html#a79646eaa928ba247387a11eb7360e5e8", null ],
-    [ "safe_ota", "trackle__utils__ota_8h.html#a855291fb065f80e9a0f8f93de3f25978", null ]
-];
